19dc9c716a87a4e1fe5f4409b6bf1657d86d0fed
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2022-01-08 00:14:29 -0300

    Create delivery route

--- src/routes.ts
+++ src/routes.ts
@@ -3,15 +3,17 @@ import { Router } from 'express'
 import { AuthenticateClientController } from './modules/account/useCases/authenticateClient/AuthenticateClientController'
 import { AuthenticateDeliverymanController } from './modules/account/useCases/authenticateDeliveryman/AuthenticateDeliverymanController'
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController'
+import { CreateDeliveryController } from './modules/deliveries/useCases/createDelivery/CreateDeliveryController'
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController'
 
 const routes = Router()
 
 const authenticateClientController = new AuthenticateClientController()
-const createClientController = new CreateClientController()
-const createDeliverymanController = new CreateDeliverymanController()
 const authenticateDeliverymanController =
   new AuthenticateDeliverymanController()
+const createClientController = new CreateClientController()
+const createDeliverymanController = new CreateDeliverymanController()
+const createDeliveryController = new CreateDeliveryController()
 
 routes.post('/client/authenticate/', authenticateClientController.handle)
 routes.post('/client/', createClientController.handle)
@@ -21,5 +23,7 @@ routes.post(
   authenticateDeliverymanController.handle
 )
 routes.post('/deliveryman/', createDeliverymanController.handle)
+
+routes.post('/delivery/', createDeliveryController.handle)
 
 export { routes }
