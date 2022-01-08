eb84b01dae28ea9b18a42d815700b7adcda22c99
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2022-01-07 23:51:41 -0300

    Express handle errors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 
+import 'express-async-errors'
 import { routes } from './routes'
 
 const app = express()
@@ -7,5 +8,18 @@ const app = express()
 app.use(express.json())
 
 app.use(routes)
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      message: err.message,
+    })
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  })
+})
 
 app.listen(3000, () => console.log('Server running on port 3000'))
