2b51250a679e28c063dc5eb2dc9330592162d68f
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2022-01-07 23:30:01 -0300

    Lint config

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
-import express from "express"
+import express from 'express'
+import { routes } from 'routes'
 
 const app = express()
 
-app.get('/', (req, res) => {
-  return res.json({
-    message: 'Hello World!',
-  })
-})
+app.use(express.json())
+
+app.use(routes)
 
 app.listen(3000, () => console.log('Server running on port 3000'))
